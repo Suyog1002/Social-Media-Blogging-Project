e3367983583c89202cd51ac05d7e2b3038b62f22
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-06-30 11:21:01 +0530

    add a controller and a router for user while linking it to home

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
+const profile=require('../controllers/pratice_controller');
 console.log('Router loaded');
 
 router.get('/',homeController.home);
+router.get('/practice',profile.practice);
+router.use('/users',require('./users'));
+
+// for any futher routes,access from here
+//router,use('/routerName',require('/routerfile'));
 module.exports=router;
