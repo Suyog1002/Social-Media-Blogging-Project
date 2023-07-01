6c05787d09d205c3953498541b6aa860cc75a3a2
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-01 12:32:42 +0530

    render sign in and sign up pages

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express=require('express');
+const cookieParse=require('cookie-parser');
 const app=express();
 const port=8000;
+// const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParse());
+
+app.use(express.static('./assets'));
+
+// app.use(expressLayouts);
 
 //use express router
-app.use('/',require('./routes'));
+app.use('/',require('./routes/index'));
 
 //set up the view engine
 app.set('view engine','ejs');
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
-const profile=require('../controllers/pratice_controller');
+
 console.log('Router loaded');
 
 router.get('/',homeController.home);
-router.get('/practice',profile.practice);
+
 router.use('/users',require('./users'));
 
 // for any futher routes,access from here
