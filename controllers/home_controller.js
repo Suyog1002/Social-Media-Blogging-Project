de7633fc381356dafa63a0267e099fcf5c287720
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-04 11:36:02 +0530

    Displayed the post and populated it's user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,23 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title: "Home"
+    // console.log(req.cookies);
+    // res.cookie('user_id',25);
+
+
+    // Post.find({}).then(function(posts,err){
+    //     return res.render('home',{
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    //populate the user of each post
+    Post.find({}).populate('user').then(function(posts,err){
+        return res.render('home',{
+            title: "Codeial | Home",
+            posts: posts
+        });
     })
 }
 
