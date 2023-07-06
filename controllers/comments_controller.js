e3ae1f67e143e5275f98a2c652be5112dd7c8763
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-06 11:49:08 +0530

    deleted the commit made by that user in the post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -22,3 +22,18 @@ module.exports.create=function(req,res){
         }
     });
 }
+
+module.exports.destroy = function(req,res){
+    Comment.findById(req.params.id).then(function(comment,err){
+        
+        if(comment.user==req.user.id ){   
+            let postId=comment.post;                                      
+            comment.deleteOne();
+            Post.findByIdAndUpdate(postId,{$pull: {comments: req.params.id}}).then(function(post,err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
