777f03cd58ae2a79eaeadb14586e1062ab8a3585
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-14 18:28:52 +0530

    install multer for taking file as input and storing in the database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -10,13 +10,41 @@ module.exports.profile=function(req,res){
     
 }
 
-module.exports.update=function(req,res){
+module.exports.update= async function(req,res){
     //no logged in user should be able to update user profile (id)
+    // if(req.user.id==req.params.id){
+    //     User.findByIdAndUpdate(req.params.id,req.body).then(function(user,err){
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     return res.status(401).send('Unauthorized');
+    // }
+
     if(req.user.id==req.params.id){
-        User.findByIdAndUpdate(req.params.id,req.body).then(function(user,err){
+        try{
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar (req,res,function(err){
+                if(err){
+                    console.log('*******Multer Error: ',err)
+                }
+                user.name=req.body.name;
+                user.email=req.body.email;
+
+                if(req.file){
+                    //this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath+ '/'+ req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+            
+
+        }catch(err){
+            req.flash('error',err);
             return res.redirect('back');
-        });
+        }
     }else{
+        req.flash('error','Unauthorized!')
         return res.status(401).send('Unauthorized');
     }
 }
