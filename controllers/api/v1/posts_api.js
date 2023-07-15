ab86d9915470fdfd95b00882a0151eb5bfea378e
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-15 13:17:57 +0530

    deleted a post using a api without authentication

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,46 @@
-module.exports.index= function(req,res){
+const Post =require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index= async function(req,res){
+    let posts=await Post.find({})
+        .sort('-createdAt') //to store in order in which it is created (new post will be shown first)
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+    });
+
     return res.json(200,{
         message: "List of posts",
-        posts: []
+        posts: posts
     })
+}
+
+module.exports.destroy = async function(req,res){
+    
+    try{
+        let post = await Post.findById(req.params.id);
+            //.id means converting the object id into string
+        // if(post.user == req.user.id){
+            post.deleteOne();
+            //deleting all comments related to the post deleted
+            await Comment.deleteMany({post: req.params.id});
+
+            
+            // req.flash('success','Post and association comments deleted');
+            return res.json(200,{
+                message: "Post and Associated comments deleted successfully!"
+            });
+        // }else{
+        //     req.flash('error','You cannot delete this post!');
+        //     return res.redirect('back'); //if current user and post user doesn't match
+        // }
+    }catch(err){
+        console.log('*********',err);
+        return res.json(500,{
+            message: "Internal Server Error"
+        });
+    }
 }
