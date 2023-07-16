8b4c29c560a220442d56733c635439f469e1e7ab
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-16 12:49:13 +0530

    Setting up passport JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const { ExtractJwt } = require('passport-jwt');
+
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey: 'codeial' //encryption and decryption string
+}
+
+passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
+    User.findById(jwtPayLoad._id).then(function(user,err){
+        if(err){console.log('Error in finding user from JWT'); return;}
+        if(user){
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    })
+}));
+
+module.exports = passport;
