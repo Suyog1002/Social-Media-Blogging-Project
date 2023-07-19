520bd067bca8cd4a113e5f0a4e7386721e0704b1
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-19 19:42:46 +0530

    setup schema for likes

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const postSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -13,6 +14,12 @@ const postSchema = new mongoose.Schema({
         {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment'
+        }
+    ],
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Like'
         }
     ]
 },{
