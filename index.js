3e8e2f30ce1612277b80c862dcd2dd58532e2920
Suyog Ambadare <suyogambadare1003@gmail.com>
2023-07-20 22:54:36 +0530

    setup Socket.io and initial connection

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const passportGoogle = require('./config/passport-google-oauth2-strategy');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const customMware =require('./config/middleware');
+
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
 // const sassMiddleware=require('sass-middleware');
 
 // app.use(sassMiddleware({
